00175e2c2f1fd80ba16bf776a35ffb3580550b71
Martin Matovu <matovius@proton.me>
2023-11-29 20:25:41 +0300

    Remove unused components

--- src/lib/components/icons/Home.svelte
+++
@@ -1,9 +0,0 @@
-<svg viewBox="0 0 24 24" fill="none" class="icon" xmlns="http://www.w3.org/2000/svg">
-	<path
-		d="M12 5C11 7 5 11 3 12L5 19H9L10 13H14C14 13 15 18.3333 15 19H19L21 12C19 11 13 7 12 5Z"
-		stroke="currentColor"
-		stroke-width="2"
-		stroke-linecap="round"
-		stroke-linejoin="round"
-	/>
-</svg>

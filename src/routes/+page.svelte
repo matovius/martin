ee73cba38bae774ffe9419da74ed9ae719616fa5
Martin Matovu <matovius@proton.me>
2023-11-29 20:29:34 +0300

    Overhaul page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,146 +1,210 @@
 <script lang="ts">
-	import profileImage from '$lib/assets/profile-photo.png';
-	import tildeLogo from '$lib/assets/Tilde-Logo.png';
-	import theWebwareWorkshop from '$lib/assets/TheWebwareWorkshopLogo.png';
-	import TildeLogo from '$lib/assets/Tilde-Logo.png';
-	import { ArrowUpRight, Codepen, Dribbble, Github, Instagram, Linkedin } from 'lucide-svelte';
-
-	const date: Date = new Date();
-	let CurrentDate = date.getFullYear();
+	import { ArrowRight, Dribbble, Github, Instagram, Linkedin } from 'lucide-svelte';
 </script>
 
 <svelte:head>
-	<title>Martin Matovu: Web Designer & Developer</title>
+	<title>Martin Matovu</title>
 </svelte:head>
 
-<div class="w-full max-w-full flex flex-col">
-	<main class="w-full h-full flex flex-col justify-center items-center">
-		<section
-			class="w-full max-w-[40rem] flex flex-col justify-center items-center gap-12 px-6 pt-[60px] pb-6"
-		>
-			<div
-				id="hero-lead"
-				class="w-full flex flex-col-reverse tablet:flex-row justify-center items-center gap-12"
-			>
-				<div id="hero-text" class="flex flex-col justify-center items-start gap-6">
-					<div id="headline" class="">
-						<span class="p">Hi, my name is</span>
-						<h1 class="h1 text-tang-blue">Martin Matovu</h1>
-					</div>
-					<div class="flex flex-row justify-start items-start gap-6 flex-wrap">
-						<span class="small">Pixel Pusher</span>
-						<span class="small">Browser Charmer</span>
-						<span class="small">API Whisperer</span>
-					</div>
-					<span class="p">More commonly known as a <strong>web developer</strong></span>
-				</div>
-				<div class="w-52 h-52 aspect-square">
-					<img
-						src={profileImage}
-						alt="Me looking sternly into the camera"
-						style="width: 100%; height: 100%;"
-					/>
-				</div>
-			</div>
-			<div id="hero-trail" class="w-full flex-col justify-start items-center">
-				<div class="w-full flex flex-row justify-center items-center gap-2 flex-wrap">
-					<a
-						href="https://github.com/matovius"
-						target="_blank"
-						rel="noopener noreferrer"
-						class="button"
-					>
-						<Github />
-						<span>GitHub</span>
-					</a>
-
-					<a
-						href="https://dribbble.com/matovius"
-						target="_blank"
-						rel="noopener noreferrer"
-						class="button"
-					>
-						<Dribbble />
-						<span>Dribbble</span>
-					</a>
-
-					<a
-						href="https://instagram.com/matovius_"
-						target="_blank"
-						rel="noopener noreferrer"
-						class="button"
-					>
-						<Instagram />
-						<span>Instagram</span>
-					</a>
-
-					<a
-						href="https://linkedin.com/in/martin-matovu/"
-						target="_blank"
-						rel="noopener noreferrer"
-						class="button"
-					>
-						<Linkedin />
-						<span>LinkedIn</span>
-					</a>
-
-					<div class="w-fit hidden">
-						<a href="https://codepen.io/" target="_blank" rel="noopener noreferrer" class="button">
-							<Codepen />
-							<span>Codepen</span>
-						</a>
-					</div>
-				</div>
-			</div>
-		</section>
-		<section class="w-full max-w-[40rem] flex justify-center items-start px-6 pt-12 pb-12">
-			<div id="about-me-text" class="w-full flex flex-col justify-start items-start gap-6">
-				<p class="p">
-					I love making beautiful websites, learning about the world around us, and doing the most
-					amateur photography you'll ever see.
-				</p>
-				<p class="p">
-					I currently work at The Webware Workshop as a web designer/developer combo. I've worked on
-					quite a few things you can check out on my GitHub, Dribbble, and across my other social
-					media profiles. All the links are provided up at the top.
-				</p>
-			</div>
-		</section>
-		<section
-			class="w-full max-w-[40rem] flex flex-col justify-start items-center gap-6 px-6 pb-12 tablet:pb-24"
-		>
-			<div
-				class="card w-full rounded-3xl p-6 flex flex-col tablet:flex-row justify-between items-center gap-6"
-			>
-				<div
-					class="w-full tablet:w-fit flex flex-row justify-center tablet:justify-start items-center gap-6"
-				>
-					<div class="w-12 h-12 aspect-square">
-						<img src={TildeLogo} alt="The Webware Workshop's Logo" class="icon" />
-					</div>
-					<div>
-						<h2 class="h5">Tilde</h2>
-						<span class="p text-white/60">Your Favorite Online Text Editor</span>
-					</div>
-				</div>
-				<div class="w-full tablet:w-fit">
-					<a
-						href="https://tilde-editor.pages.dev/"
-						target="_blank"
-						rel="noopener noreferrer"
-						class="button w-full tablet:w-fit"
-					>
-						<span>Check it out</span>
-						<ArrowUpRight />
-					</a>
-				</div>
-			</div>
-		</section>
-	</main>
-	<footer class="w-full flex justify-center items-center p-6">
-		<div id="footer-text" class="w-full max-w-[53.75rem] flex flex-row justify-center item-center">
-			<small class="small">© Copyright 2023 · Martin Matovu</small>
+<main class="main">
+	<section id="hero" class="hero">
+		<div class="headline">
+			<h1 class="h1">I'm Martin Matovu</h1>
+			<h2 class="h2">A freelance web designer and developer</h2>
 		</div>
-		<div />
-	</footer>
-</div>
+		<div class="contact-cta">
+			<div class="availability">
+				<div class="indicator" data-available="true" />
+			</div>
+			<button class="button">
+				<span>Get In Touch</span>
+				<ArrowRight />
+			</button>
+		</div>
+		<div class="social-buttons">
+			<button class="button github">
+				<Github />
+				<span>GitHub</span>
+			</button>
+			<button class="button dribbble">
+				<Dribbble />
+				<span>Dribbble</span>
+			</button>
+			<button class="button instagram">
+				<Instagram />
+				<span>Instagram</span>
+			</button>
+			<button class="button linkedin">
+				<Linkedin />
+				<span>LinkedIn</span>
+			</button>
+		</div>
+	</section>
+	<section id="about" class="about">
+		<p class="p">
+			I love making the web more beautiful, learning about the world around us, and doing the most
+			amateur photography you'll ever see.
+		</p>
+		<p class="p">
+			I've worked on quite a few things, but that doesn't mean I don't know how to play. Sometimes,
+			I even make wallpapers too.
+		</p>
+		<p class="p">
+			I also have a little blog where I share some of my thoughts and details about the projects I
+			work on. I don't write very often, but I try to write when I can. But you can check it out
+			nonetheless.
+		</p>
+	</section>
+</main>
+
+<style>
+	.main {
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		justify-content: flex-start;
+		align-items: center;
+
+		& > section {
+			width: 100%;
+			max-width: 40rem; /* 640px */
+			display: flex;
+			flex-direction: column;
+			justify-content: flex-start;
+			align-items: center;
+			gap: 24px; /* 24px */
+			padding: 1.5rem; /* 24px */
+		}
+
+		& > .hero#hero {
+			gap: 3rem; /* 48px */
+		}
+	}
+
+	.headline {
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		gap: 1.5rem; /* 24px */
+
+		& > h1.h1 {
+			color: hsl(216, 90%, 46%);
+		}
+
+		& > h2.h2 {
+			color: hsl(0, 0%, 0%, 0.6);
+		}
+	}
+
+	.contact-cta {
+		width: 100%;
+		display: flex;
+		flex-direction: row;
+		justify-content: flex-start;
+		align-items: center;
+		gap: 24px; /* 1.5rem */
+
+		& > .availability {
+			width: 90px;
+			display: flex;
+			justify-content: flex-start;
+			align-items: center;
+			gap: 0.75rem; /* 12px */
+
+			& > .indicator {
+				width: 12px;
+				height: 12px;
+				aspect-ratio: 1;
+				display: flex;
+				justify-content: center;
+				align-items: center;
+				border-radius: 9999rem;
+				position: relative;
+
+				&::after {
+					/*
+					position: absolute;
+					left: calc(100% + 0.75rem);*/
+					font-size: var(--font-size-small);
+					color: hsl(0, 0%, 0%, 0.8);
+					transform: translateX(80%);
+				}
+
+				&[data-available='true'] {
+					background-color: hsl(111, 73%, 43%);
+
+					&::after {
+						content: 'Available';
+					}
+				}
+
+				&[data-available='false'] {
+					background-color: hsl(0, 0%, 0%, 0.4);
+
+					&::after {
+						content: 'Not Available';
+					}
+				}
+			}
+		}
+
+		& > .button {
+			border-color: hsl(216, 90%, 46%, 0.2);
+			color: hsl(216, 90%, 46%);
+
+			&:hover {
+				color: hsl(0, 0%, 100%);
+				background-color: hsl(216, 90%, 46%);
+			}
+		}
+	}
+
+	.social-buttons {
+		width: 100%;
+		display: flex;
+		flex-direction: row;
+		flex-wrap: wrap;
+		justify-content: flex-start;
+		align-items: center;
+		gap: 0.5rem; /* 8px */
+
+		& > .github {
+			border-color: hsl(0, 0%, 0%, 0.1);
+			color: hsl(0, 0%, 0%);
+
+			&:hover {
+				color: hsl(0, 0%, 100%);
+				background-color: hsl(0, 0%, 0%);
+			}
+		}
+
+		& > .dribbble {
+			border-color: hsl(313, 40%, 49%, 0.2);
+			color: hsl(313, 40%, 49%);
+			&:hover {
+				color: hsl(0, 0%, 100%);
+				background-color: hsl(313, 40%, 49%);
+			}
+		}
+
+		& > .instagram {
+			border-color: hsl(28, 97%, 54%, 0.2);
+			color: hsl(28, 97%, 54%);
+
+			&:hover {
+				color: hsl(0, 0%, 100%);
+				background-color: hsl(28, 97%, 54%);
+			}
+		}
+
+		& > .linkedin {
+			border-color: hsla(221, 89%, 42%, 0.2);
+			color: hsl(221, 89%, 42%);
+
+			&:hover {
+				color: hsl(0, 0%, 100%);
+				background-color: hsl(221, 89%, 42%);
+			}
+		}
+	}
+</style>

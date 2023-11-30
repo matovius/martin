7641d49bb530108118fb6d296cfa1400efc6a54a
Martin Matovu <matovius@proton.me>
2023-11-30 12:56:54 +0300

    Add link to homepage

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -4,9 +4,11 @@
 
 <header class="header">
 	<div class="container">
-		<div class="icon">
-			<Logo />
-		</div>
+		<a href="/" class="home-link">
+			<div class="icon">
+				<Logo />
+			</div>
+		</a>
 	</div>
 </header>
 
@@ -25,17 +27,37 @@
 			flex-direction: row;
 			justify-content: start;
 			align-items: center;
+		}
+	}
 
-			& > .icon {
-				width: 48px; /* 48px */
-				height: 48px; /* 48px */
-				aspect-ratio: 1;
-				color: hsl(216, 90%, 46%);
+	.home-link {
+		width: fit-content;
+		height: fit-content;
+		border-radius: 0.5rem; /* 8px */
+		outline: 0.125rem solid transparent; /* 2px */
+		outline-offset: 0.25rem; /* 4px */
 
-				@media (min-width: 480px) {
-					width: 6rem; /* 96px */
-					height: 6rem; /* 96px */
-				}
+		&:hover {
+			opacity: 0.6;
+		}
+
+		&:focus-visible {
+			outline-color: currentColor;
+		}
+
+		&:visited {
+			color: unset;
+		}
+
+		& > .icon {
+			width: 48px; /* 48px */
+			height: 48px; /* 48px */
+			aspect-ratio: 1;
+			color: hsl(216, 90%, 46%);
+
+			@media (min-width: 480px) {
+				width: 6rem; /* 96px */
+				height: 6rem; /* 96px */
 			}
 		}
 	}

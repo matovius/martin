bd2979297abef9de6f5e95ba3773d9d2def004d8
Martin Matovu <matovius@proton.me>
2024-05-18 12:32:56 +0300

    Add component

---
+++ src/lib/components/FeaturedProjectCard.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+	export let heading: string = 'Heading';
+	export let details: string = 'Details go here';
+	export let url: string = 'https://example.com';
+</script>
+
+<a href={url} target="_blank" rel="noopener noreferrer" class="card">
+	<h5 class="heading">
+		{heading}
+	</h5>
+	<p class="details">
+		{details}
+	</p>
+</a>
+
+<style>
+	.card {
+		appearance: none;
+		color: unset;
+		text-decoration: none;
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		padding: 20px;
+		border-radius: 20px;
+		outline: 2px solid transparent;
+		outline-offset: 4px;
+
+		& > .heading {
+			font-size: 30px;
+			font-weight: 400;
+			line-height: 1.5;
+			text-decoration: 2px solid underline;
+			text-underline-offset: 4px;
+		}
+
+		&:is(:hover, :focus) {
+			background: hsl(var(--clr-text), 0.1);
+
+			& > .heading {
+				color: hsl(var(--clr-primary));
+			}
+		}
+
+		&:focus-visible {
+			outline-color: hsl(var(--clr-primary));
+		}
+	}
+</style>

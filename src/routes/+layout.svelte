f853fb676ea1a9bf759656d92bd6bc4e3b239462
Martin Matovu <matovius@proton.me>
2024-05-18 15:22:18 +0300

    Add new fonts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
+	import '@fontsource/passion-one/400.css';
+	import '@fontsource/passion-one/700.css';
+	import '@fontsource-variable/inter';
 	import '../app.css';
-	import '@fontsource-variable/rubik';
 
 	import Footer from '$lib/components/Footer.svelte';
 	import Header from '$lib/components/Header.svelte';
@@ -15,7 +17,6 @@
 <style>
 	.root {
 		isolation: isolate;
-		position: relative;
-		min-height: 100dvh;
+		width: 100dvw;
 	}
 </style>

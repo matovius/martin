efb3575621e9e8063ab17c5c6b04a296680006f6
Martin Matovu <matovius@proton.me>
2024-06-04 19:55:53 +0300

    Add coming soon component

---
+++ src/lib/components/ComingSoon.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+</script>
+
+<section id="coming-soon" class="coming-soon">
+	<div class="container">
+		<div class="headline">
+			<h1 class="h1 heading">Coming Soon</h1>
+			<h2 class="h4 subheading">
+				This page is currently under development and will be coming soon
+			</h2>
+		</div>
+	</div>
+</section>
+
+<style>
+	.coming-soon {
+		width: 100%;
+		height: calc(80dvh - 70px);
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+
+		& > .container {
+			padding-bottom: 40px;
+
+			& > .headline {
+				display: flex;
+				flex-direction: column;
+				gap: 20px;
+
+				& > .heading {
+					color: hsl(var(--clr-primary));
+				}
+			}
+		}
+	}
+</style>
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,3 +2,4 @@ export * from './components/Logo.svelte';
 export * from './components/Header.svelte';
 export * from './components/Footer.svelte';
 export * from './components/FeaturedProjectCard.svelte';
+export * from './components/ComingSoon.svelte';

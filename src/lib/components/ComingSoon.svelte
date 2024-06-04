7abcf5697c7814418afc73d6067e0969b6dd988f
Martin Matovu <matovius@proton.me>
2024-06-04 20:10:43 +0300

    Expand coming soon component

--- src/lib/components/ComingSoon.svelte
+++ src/lib/components/ComingSoon.svelte
@@ -6,8 +6,21 @@
 		<div class="headline">
 			<h1 class="h1 heading">Coming Soon</h1>
 			<h2 class="h4 subheading">
-				This page is currently under development and will be coming soon
+				This page is currently under construction and will be coming soon, but don't hold your
+				breath.
 			</h2>
+			<p class="p">In the meantime...</p>
+			<button
+				class="button exorcism"
+				title="I cast thee back to whence thou hath comest"
+				on:click={() => {
+					history.back();
+				}}
+			>
+				<div class="front">
+					<span>Go back</span>
+				</div>
+			</button>
 		</div>
 	</div>
 </section>
@@ -15,11 +28,12 @@
 <style>
 	.coming-soon {
 		width: 100%;
-		height: calc(80dvh - 70px);
+		height: calc(90dvh - 70px);
 		display: flex;
 		flex-direction: column;
 		justify-content: center;
 		align-items: center;
+		padding-inline: 20px;
 
 		& > .container {
 			padding-bottom: 40px;

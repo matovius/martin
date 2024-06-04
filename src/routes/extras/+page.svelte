130abd0dd18d927849a3ab1dd53a4f14ce52ff0b
Martin Matovu <matovius@proton.me>
2024-06-04 20:23:30 +0300

    Add extras page

---
+++ src/routes/extras/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import ComingSoon from '$lib/components/ComingSoon.svelte';
+
+	let showPage: boolean = false;
+
+	onMount(() => {
+		setTimeout(() => {
+			showPage = true;
+		}, 200);
+	});
+</script>
+
+<svelte:head>
+	<title>Anything Extra - Martin Matovu</title>
+</svelte:head>
+
+{#if showPage}
+	<ComingSoon />
+{/if}

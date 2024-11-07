88fee2037ae67d397690b0935e8630a6e06550e7
Martin Matovu <matovius@proton.me>
2024-11-07 03:02:00 +0300

    remove unnecessary file

--- src/lib/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './components/Logo.svelte';
-export * from './components/Header.svelte';
-export * from './components/Footer.svelte';
-export * from './components/FeaturedProjectCard.svelte';
-export * from './components/ComingSoon.svelte';

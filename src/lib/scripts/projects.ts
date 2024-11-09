0e14572bf1beafcc1b48b2e5ac19197996b81328
Martin Matovu <matovius@proton.me>
2024-11-09 09:43:58 +0300

    add project data

---
+++ src/lib/scripts/projects.ts
@@ -0,0 +1,21 @@
+export const projects = [
+	{
+		name: 'Built By',
+		year: 2024,
+		description: 'For when you want some cretive adjectives for your project footers.',
+		url: 'https://built-by.pages.dev'
+	},
+	{
+		name: 'psswrds',
+		year: 2024,
+		description: 'A simple password generator that uses sweet animations to reveal them.',
+		url: 'https://psswrds.pages.dev'
+	},
+	{
+		name: 'Moments',
+		year: 2024,
+		description:
+			'A simple journaling app for taking unstructured notes and keeping track of your life.',
+		url: 'https://moments-app.pages.dev'
+	}
+];

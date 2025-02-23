3e5af2dfc2735e77b7cb96fe81384a6562833463
Martin Matovu <matovius@proton.me>
2025-02-24 02:07:41 +0300

    feat: overhaul `FeaturedProjectCard` component structure and styles to make it look better

--- src/lib/components/FeaturedProjectCard.svelte
+++ src/lib/components/FeaturedProjectCard.svelte
@@ -8,11 +8,11 @@
 	let { heading = 'Heading', year = 1970, url = 'https://example.com' }: Props = $props();
 </script>
 
-<a href={url} target="_blank" rel="noopener noreferrer" class="card">
-	<h5 class="h5">
+<a href={url} target="_blank" rel="noopener noreferrer" class="card" aria-label="{heading}; made in {year}">
+	<div class="h5" aria-hidden="true">
 		{heading}
-	</h5>
-	<p class="p">{year}</p>
+	</div>
+	<small aria-hidden="true">{year}</small>
 </a>
 
 <style>
@@ -35,32 +35,37 @@
 
 		&::before {
 			content: '';
+			width: 0px;
+			height: auto;
+			aspect-ratio: 1;
+			border-radius: 50%;
 			position: absolute;
-			inset: 0;
+			inset-inline-start: -1.25rem; /* -20px */
 			background: var(--clr-primary);
-			transform: translateX(-100%);
 		}
 
 		& > * {
 			position: absolute;
 		}
-		& > h5 {
-			inset-inline-start: 0;
+		& > .h5 {
+			font-family: var(--font-passion-one);
+			inset-inline-start: .3125rem; /* 5px */
 		}
-		& > p {
-			inset-inline-end: 20px;
+		& > small {
+			inset-inline-end: 1.25rem; /* 20px */
+			opacity: 0.6;
 		}
 
 		&:is(:global(:hover, :focus)) {
 			color: var(--clr-base);
 
 			&::before {
-				transform: translateX(0);
+				width: 110%;
 				transition-delay: 200ms;
 			}
 
-			& > h5 {
-				transform: translateX(20px);
+			& > .h5 {
+				transform: translateX(1.25rem); /* 20px */
 			}
 		}
 

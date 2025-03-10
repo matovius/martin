9f54ac4bf7db840fc736f6de4c960ad894093f5f
Martin Matovu <matovius@proton.me>
2025-03-10 13:48:13 +0300

    feat: populate projects page

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import ComingSoon from '$lib/components/ComingSoon.svelte';
+	import { quadInOut } from 'svelte/easing';
+	import { fade, fly } from 'svelte/transition';
+	import { projects } from '$lib/scripts/projects';
+	import ProjectCard from '$lib/components/ProjectCard.svelte';
 
 	let showPage: boolean = $state(false);
 
@@ -16,5 +19,70 @@
 </svelte:head>
 
 {#if showPage}
-	<ComingSoon />
+	<main
+		class="main"
+		in:fly={{ duration: 200, easing: quadInOut, x: 0, y: 50, opacity: 0 }}
+		out:fade={{ duration: 200, easing: quadInOut }}
+	>
+		<section id="all-projects" class="all-projects">
+			<div class="container">
+				<div class="headline">
+					<h1 class="heading">My Projects</h1>
+					<p>
+						I specialize in making the simplest, most <em>littlest</em> tools imaginable,
+						and also do freelance work through Webware Studio.
+					</p>
+				</div>
+
+				<ul class="projects-list">
+					{#each projects as project}
+						<li>
+							<ProjectCard {project} />
+						</li>
+					{/each}
+				</ul>
+			</div>
+		</section>
+	</main>
 {/if}
+
+<style>
+	.main {
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding-inline: 20px;
+		padding-block: 120px 40px;
+	}
+	
+	section {
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+
+		& > .container {
+			padding-block-end: 40px;
+
+			& > .headline {
+				display: flex;
+				flex-direction: column;
+				gap: 20px;
+
+				& > .heading {
+					color: var(--clr-primary);
+				}
+			}
+		}
+	}
+
+	ul.projects-list {
+		list-style: none;
+		display: grid;
+		gap: .75rem; /* 12px */
+		margin-top: 3.75rem; /* 60px */
+	}
+</style>

ae613f6ebefb9b516e84444db59ea756f8953325
Martin Matovu <matovius@proton.me>
2025-03-10 13:45:44 +0300

    feat: add optional icon and image keys to `Project` type

--- src/lib/scripts/types.ts
+++ src/lib/scripts/types.ts
@@ -1,8 +1,13 @@
+/**
+ * The structure of a project object
+ */
 export type Project = {
 	name: string;
 	description: string;
 	year: number;
 	url: string;
+	icon?: string;
+	image?: string;
 	isOngoing?: boolean;
 	isFeatured?: boolean;
 };

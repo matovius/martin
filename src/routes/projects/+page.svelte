5f28e039331212bd9b1e576514db93c2c92aef94
Martin Matovu <matovius@proton.me>
2025-06-28 19:27:18 +0300

    refactor: rework page elements and text, change page title

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import { quadInOut } from 'svelte/easing';
-	import { fade, fly } from 'svelte/transition';
 	import { projects } from '$lib/scripts/projects';
 	import ProjectCard from '$lib/components/ProjectCard.svelte';
 
@@ -15,36 +13,30 @@
 </script>
 
 <svelte:head>
-	<title>My Projects - Martin Matovu</title>
+	<title>Projects - Martin Matovu</title>
 </svelte:head>
 
-{#if showPage}
-	<main
-		class="main"
-		in:fly={{ duration: 200, easing: quadInOut, x: 0, y: 50, opacity: 0 }}
-		out:fade={{ duration: 200, easing: quadInOut }}
-	>
-		<section id="all-projects" class="all-projects">
-			<div class="container">
-				<div class="headline">
-					<h1 class="heading">My Projects</h1>
-					<p>
-						I specialize in making the simplest, most <em>littlest</em> tools imaginable,
-						and also do freelance work through Webware Studio.
-					</p>
-				</div>
-
-				<ul class="projects-list">
-					{#each projects as project}
-						<li>
-							<ProjectCard {project} />
-						</li>
-					{/each}
-				</ul>
+<main class="main">
+	<section id="all-projects" class="all-projects">
+		<div class="container">
+			<div class="headline">
+				<h1 class="heading">Projects</h1>
+				<p>
+					I specialize in making the simplest, most <em>littlest</em> tools imaginable, and also do freelance
+					work through Webware Studio.
+				</p>
 			</div>
-		</section>
-	</main>
-{/if}
+
+			<ul class="projects-list">
+				{#each projects as project}
+					<li>
+						<ProjectCard {project} />
+					</li>
+				{/each}
+			</ul>
+		</div>
+	</section>
+</main>
 
 <style>
 	.main {
@@ -56,7 +48,7 @@
 		padding-inline: 20px;
 		padding-block: 120px 40px;
 	}
-	
+
 	section {
 		width: 100%;
 		display: flex;
@@ -73,7 +65,7 @@
 				gap: 20px;
 
 				& > .heading {
-					color: var(--clr-primary);
+					color: var(--color-primary);
 				}
 			}
 		}
@@ -82,7 +74,6 @@
 	ul.projects-list {
 		list-style: none;
 		display: grid;
-		gap: .75rem; /* 12px */
-		margin-top: 3.75rem; /* 60px */
+		padding-block-start: 3.75rem; /* 60px */
 	}
 </style>

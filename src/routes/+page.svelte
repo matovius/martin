1f07590900c3327b754e869e19b86c6af283b537
Martin Matovu <matovius@proton.me>
2025-06-28 19:26:11 +0300

    refactor: rework page elements, use snippets for page sections, change page title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,41 +1,97 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	import { quadInOut } from 'svelte/easing';
-	import { fade, fly } from 'svelte/transition';
-	import Hero from './Hero.svelte';
-	import FeaturedProjects from './FeaturedProjects.svelte';
-
-	let showPage: boolean = $state(false);
-
-	onMount(() => {
-		setTimeout(() => {
-			showPage = true;
-		}, 200);
-	});
+	import ProjectCard from '$lib/components/FeaturedProjectCard.svelte';
+	import { projects } from '$lib/scripts/projects';
 </script>
 
 <svelte:head>
-	<title>Martin Matovu - Welcome to my Home</title>
+	<title>Martin Matovu</title>
 </svelte:head>
-{#if showPage}
-	<main
-		class="main"
-		in:fly={{ duration: 200, easing: quadInOut, x: 0, y: 50, opacity: 0 }}
-		out:fade={{ duration: 200, easing: quadInOut }}
-	>
-		<Hero />
-		<FeaturedProjects />
-	</main>
-{/if}
+
+<main>
+	{@render HeroSection()}
+	{@render ProjectsSection()}
+</main>
+
+{#snippet HeroSection()}
+	<section id="hero" class="hero">
+		<div class="container">
+			<div class="headline">
+				<h1 class="heading">Hi, I'm Martin Matovu</h1>
+				<h2 class="h4 subheading">Frontend engineer + Accessibility acolyte</h2>
+				<p class="p text">
+					I make browsers do my bidding for my enjoyment and can write some mean JavaScript. I'm
+					part of the duo at <a
+						href="https://webware.studio"
+						target="_blank"
+						rel="noopener noreferrer"
+						class="link external">Webware Studio</a
+					>, an upstart web dev studio building incredible projects using SvelteKit.
+				</p>
+			</div>
+		</div>
+	</section>
+{/snippet}
+
+{#snippet ProjectsSection()}
+	<section id="projects" class="projects">
+		<div class="container">
+			<div class="headline">
+				<h3 class="heading">Some recent projects...</h3>
+			</div>
+
+			<ul class="featured-projects">
+				{#each projects as project}
+					{#if project.isFeatured}
+						<li>
+							<ProjectCard {project} />
+						</li>
+					{/if}
+				{/each}
+			</ul>
+
+			<div class="cta">
+				<a href="/projects" class="btn more-projects" aria-label="See more projects"></a>
+			</div>
+		</div>
+	</section>
+{/snippet}
 
 <style>
-	.main {
-		width: 100%;
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding-inline: 20px;
-		padding-block: 120px 40px;
+	main {
+		padding-inline: 1.25rem /* 20px */;
+		padding-block: 3.75rem /* 60px*/ 2.5rem /* 40px */;
+		margin-block-start: var(--header-height);
+
+		@media screen and (min-width: 37.5rem) /* 600px */ {
+			padding-block-start: 7.5rem /* 120px */;
+		}
+	}
+
+	section {
+		& > .container {
+			padding-block-end: 2.5rem /* 40px */;
+			margin-inline: auto;
+
+			& > .headline {
+				display: flex;
+				flex-direction: column;
+				gap: 1.25rem /* 20px */;
+
+				& > .heading {
+					color: var(--color-primary);
+				}
+			}
+
+			& > .cta {
+				display: flex;
+				padding-block-start: 1.25rem /* 20px */;
+			}
+		}
+	}
+
+	ul.featured-projects {
+		list-style: none;
+		display: grid;
+		padding-block-start: 1.25rem /* 20px */;
 	}
 </style>

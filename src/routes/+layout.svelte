25d650328c9bb478fbdc61b2d372690e7d776f85
Martin Matovu <matovius@proton.me>
2025-06-28 19:22:15 +0300

    fix: remove unnecessary root div, change up font imports, make `children` prop properly type-safe

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,29 +1,21 @@
 <script lang="ts">
-	import '@fontsource/passion-one/400.css';
-	import '@fontsource/passion-one/700.css';
+	import '@fontsource/passion-one';
 	import '@fontsource-variable/inter';
+	import '@fontsource-variable/jetbrains-mono';
 	import '../app.css';
 
 	import Footer from '$lib/components/Footer.svelte';
 	import Header from '$lib/components/Header.svelte';
-	
+
 	interface Props {
-		children: any;
+		children: import('svelte').Snippet;
 	}
 
 	let { children }: Props = $props();
 </script>
 
-<div id="root" class="root">
-	<Header />
+<Header />
 
-	{@render children()}
+{@render children()}
 
-	<Footer />
-</div>
-
-<style>
-	.root {
-		isolation: isolate;
-	}
-</style>
+<Footer />

aa684e7834082f36e489d85018dd31e01904835a
Martin Matovu <matovius@proton.me>
2025-06-28 19:32:57 +0300

    refactor: completely rework `ProjectCard` component layout and styling

--- src/lib/components/ProjectCard.svelte
+++ src/lib/components/ProjectCard.svelte
@@ -1,100 +1,77 @@
 <script lang="ts">
-	import type { Project } from "$lib/scripts/types";
+	import type { Project } from '$lib/scripts/types';
 
 	interface Props {
-    project: Project;
+		project: Project;
 	}
 
 	let { project }: Props = $props();
 </script>
 
-<a href={project.url} target="_blank" rel="noopener noreferrer" class="card"
-  aria-label="{project.name}; made in {project.year}">
-  <div class="headline">
-    {#if project.icon}
-      <div class="project-icon">
-        <img src={project.icon} alt="" />
-      </div>
-    {/if}
-    <h2 class="h3">{project.name}</h2>
-  </div>
+<a
+	href={project.url}
+	target="_blank"
+	rel="noopener noreferrer"
+	class="card"
+	aria-label="{project.name}; made in {project.year}"
+>
+	<div class="h4" aria-hidden="true">{project.name}</div>
+
+	{#if project.soundbite}
+		<p>{project.soundbite}</p>
+	{:else}
+		<p>{project.description}</p>
+	{/if}
+
 	<small aria-hidden="true">
-    {project.year}
-    {#if project.isOngoing}
-      <span>(Ongoing)</span>
-    {/if}
-  </small>
-  <p>{project.description}</p>
+		{project.year}
+	</small>
 </a>
 
 <style>
 	a.card {
 		appearance: none;
-		color: var(--clr-text);
+		color: unset;
 		text-decoration: none;
 		isolation: isolate;
 		width: 100%;
+		padding: 1.25rem /* 20px */;
 		display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    padding: 1.25rem; /* 20px */
-		border-radius: 1.25rem; /* 20px */
+		flex-direction: column;
+		gap: 0.3125rem /* 5px */;
+		border-radius: 1.5625rem /* 25px */;
 		outline: 2px solid transparent;
 		outline-offset: 2px;
 		position: relative;
-		overflow: hidden;
 		user-select: none;
 
 		&::before {
 			content: '';
-      width: 100%;
-      height: 100%;
 			border-radius: inherit;
-			background: var(--clr-primary);
 			position: absolute;
-      transform: translateY(100%);
-      z-index: -1;
-
+			inset: 0.625rem; /* 10px */
+			background: var(--color-primary);
+			z-index: -1;
+			opacity: 0;
 		}
-    & > * {
-      width: 100%;
-    }
+		& > .h4 {
+			font-family: var(--font-passion-one);
+		}
 		& > small {
 			opacity: 0.6;
 		}
 
-		&:is(:global(:hover, :focus)) {
-      color: var(--clr-base);
+		&:is(:hover, :focus) {
+			color: var(--color-base);
 
 			&::before {
-        transform: translateY(0);
-				transition-delay: 200ms;
+				inset: 0;
+				opacity: 1;
 			}
 		}
 
 		&:focus-visible {
-			outline-color: var(--clr-primary);
+			outline-color: var(--color-primary);
 		}
 	}
-
-  .headline {
-    width: 100%;
-    display: flex;
-    flex-direction: row;
-    gap: 1.25rem; /* 20px */
-  }
-  .project-icon {
-    width: 3.125rem; /* 50px */
-    height: 3.125rem; /* 50px */
-    aspect-ratio: 1;
-    border-radius: .75rem; /* 12px */
-    background: color-mix(in oklab, var(--clr-text) 50%, transparent);
-
-    & > img {
-      width: 100%;
-      height: 100%;
-      object-fit: cover;
-    }
-  }
 </style>

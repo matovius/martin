b135bcac77dd2d2441adde0e8bd0a0431c43ce90
Martin Matovu <matovius@proton.me>
2025-07-12 00:16:52 +0300

    feat: add `BlogPostCard` component

---
+++ src/lib/components/BlogPostCard.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	interface Props {
+		post: BlogPost;
+	}
+	let { post }: Props = $props();
+</script>
+
+{#key post.slug}
+	<article class="card">
+		<h3 class="title" data-toc-ignore>{post.title}</h3>
+		<p class="description">{post.description}</p>
+		<a href="/blog/{post.slug}" class="link">Read more</a>
+	</article>
+{/key}

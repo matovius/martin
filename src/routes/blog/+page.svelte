1f6a8b0d20d12619ae7ea566aed6d4c5f51f4cee
Martin Matovu <matovius@proton.me>
2025-07-12 00:43:23 +0300

    feat: insert padding details into blog page using `main` element and selector

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -15,6 +15,22 @@
 	<title>Blog - Martin Matovu</title>
 </svelte:head>
 
-{#if showPage}
+<main>
 	<ComingSoon />
-{/if}
+</main>
+
+<style>
+	main {
+		--_pblock: 2.5rem /* 40px */;
+		--_pblock-start: var(--_pblock);
+		width: 100%;
+		min-height: 90svh;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding-inline: 1.25rem /* 20px */;
+		padding-block: var(--_pblock);
+		padding-block-start: calc(var(--header-height) + var(--_pblock-start)) /* 40px */;
+	}
+</style>

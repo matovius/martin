75254f9a45ac0fc9e547d7a603bb46bc0bc7a601
Martin Matovu <matovius@proton.me>
2025-11-06 15:31:14 +0300

    chore: remove unused & deprecated files

--- src/routes/blog/+page.server.ts
+++
@@ -1,7 +0,0 @@
-import type { ServerLoadEvent } from '@sveltejs/kit';
-
-export async function load({ fetch }: ServerLoadEvent) {
-	const response = await fetch('/api/blog/posts');
-	const posts: BlogPost[] = await response.json();
-	return { posts };
-}

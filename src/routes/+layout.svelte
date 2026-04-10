729d04739e76f54afecb50055d8677d21ccdb2ca
Martin Matovu <martin@matovius.dev>
2026-04-10 16:16:02 +0300

    chore: update website icons
    - update favicon imports for `png` and `svg` versions
    - update metadata links for all available favicons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,8 @@
 	import '../app.css';
 	import '@fontsource-variable/google-sans-code';
 	import '@fontsource-variable/asap';
-	import favicon from '$lib/assets/favicon.svg';
+	import faviconSvg from '$lib/assets/favicon.svg';
+	import faviconPng from '$lib/assets/favicon.png';
 	import Header from '$components/header/Header.svelte';
 	import Footer from '$components/footer/Footer.svelte';
 
@@ -16,7 +17,9 @@
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" href="/favicon.ico" type="image/x-icon" />
+	<link rel="icon" href={faviconPng} type="image/png" />
+	<link rel="icon" href={faviconSvg} type="image/svg+xml" />
 </svelte:head>
 
 <Header />

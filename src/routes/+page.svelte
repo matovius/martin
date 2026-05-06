8d6f1caecf04c31ddd742f9970e40b61f3e858cf
Martin Matovu <martin@matovius.dev>
2026-05-06 09:55:55 +0300

    feat: update homepage
    - add work links to show off my work
    - edit hero content to be more concise
    - change copy button background color on hover

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,6 +15,39 @@
 			}, 2000);
 		}
 	});
+
+	const WorkLinks = [
+		{
+			label: 'Protracted',
+			description: "The world's first ever link lengthener",
+			url: 'https://protracted.website'
+		},
+		{
+			label: 'Umbo',
+			description: 'A simple yet robust email verification service',
+			url: 'https://umbo.email'
+		},
+		{
+			label: 'My Lab',
+			description: 'The place where I experiment with UI and other technical tests in Svelte',
+			url: 'https://lab.matovius.dev'
+		},
+		{
+			label: 'QuAr',
+			description: 'A QR code generator I made to share text from my computer to my phone',
+			url: 'https://qr.matovius.dev'
+		},
+		{
+			label: 'Ototune',
+			description: 'A web-based synth sound generator',
+			url: 'https://ototune.matovius.dev'
+		}
+		// {
+		// 	label: 'My design gallery',
+		// 	description: "The place where I collect all the various designs I've ever made",
+		// 	url: 'https://design.matovius.dev'
+		// }
+	];
 </script>
 
 <svelte:head>
@@ -32,7 +65,7 @@
 
 <main>
 	{@render HeroSection()}
-	<!-- {@render SocialsSection()} -->
+	{@render WorkSection()}
 </main>
 
 {#snippet HeroSection()}
@@ -45,21 +78,40 @@
 					Web developer && co-founder of
 					<Link as="outbound" url="https://webware.studio">Webware Studio</Link>
 				</p> -->
-				<p>Web developer.</p>
+				<p>Web developer and sometimes designer.</p>
+				<p>
+					I love making cool websites and tiny web tools and experiments that especially cater for
+					my own personal issues or just for fun.
+				</p>
+				<p>
+					I also exist on
+					<Link as="outbound" url="https://github.com/matovius">GitHub</Link> (<em>for now</em>) and
+					<Link as="outbound" url="https://mastodon.social/@matovius">Mastodon</Link>. My handle is
+					<span class="copy-button">
+						<button
+							class="btn inline"
+							aria-label="matovius"
+							onclick={() => {
+								copyText('matovius').then(() => {
+									isHandleCopied = true;
+								});
+							}}
+						></button>
+						{#if isHandleCopied}
+							<small transition:fly={{ duration: 150, y: -4, easing: quadOut }}>Copied</small>
+						{/if}
+					</span> on both.
+				</p>
 			</hgroup>
-			<p>
-				I love making cool websites and tiny web tools that especially cater for my own personal
-				issues or just for a joke.
-			</p>
 			<!-- <p>
 				So far, I have a few tools I've built, but I haven't yet integrated them into this website.
 				But when that's done you'll be able to find them
 				<Link as="inbound" url="/tools">here</Link>.
 			</p> -->
-			<p>
+			<!-- <p>
 				I'm intentionally keeping this website as simple as possible because I'm focusing more on
 				other stuff.
-			</p>
+			</p> -->
 			<!-- <p>
 				Oh, I also do
 				<Link as="inbound" url="/writing">write here</Link>
@@ -67,55 +119,29 @@
 				are mostly short and to the point, although I can sometimes go overboard if I'm super passionate
 				and inspired.
 			</p> -->
-			<p>
-				I also exist on
-				<Link as="outbound" url="https://github.com/matovius">GitHub</Link> (<em>for now</em>) and
-				<Link as="outbound" url="https://mastodon.social/@matovius">Mastodon</Link>. My handle is
-				<span class="copy-button">
-					<button
-						class="btn inline"
-						aria-label="matovius"
-						onclick={() => {
-							copyText('matovius').then(() => {
-								isHandleCopied = true;
-							});
-						}}
-					></button>
-					{#if isHandleCopied}
-						<small transition:fly={{ duration: 150, y: -4, easing: quadOut }}>Copied</small>
-					{/if}
-				</span> on both.
-			</p>
 		</div>
 	</section>
 {/snippet}
 
-<!-- {#snippet SocialsSection()}
-	<section id="socials" class="socials-section">
+{#snippet WorkSection()}
+	<section id="work" class="work">
 		<div class="container">
-			<p>
-				Oh yeah! I also exist on
-				<Link as="outbound" url="https://github.com/matovius">GitHub</Link>
-				and
-				<Link as="outbound" url="https://mastodon.social/@matovius">Mastodon</Link>. My handle is
-				<span class="copy-button">
-					<button
-						class="btn inline"
-						aria-label="matovius"
-						onclick={() => {
-							copyText('matovius').then(() => {
-								isHandleCopied = true;
-							});
-						}}
-					></button>
-					{#if isHandleCopied}
-						<small transition:fly={{ duration: 150, y: -4, easing: quadOut }}>Copied</small>
-					{/if}
-				</span> on both.
-			</p>
+			<h2>Previous work</h2>
+			<ul class="work-list">
+				{#each WorkLinks as link, index (WorkLinks[index])}
+					<li>
+						<article>
+							<h3 class="h5">
+								<Link as="outbound" url={link.url}>{link.label}</Link>
+							</h3>
+							<p>{link.description}</p>
+						</article>
+					</li>
+				{/each}
+			</ul>
 		</div>
 	</section>
-{/snippet} -->
+{/snippet}
 
 <style>
 	main {
@@ -145,6 +171,10 @@
 		transform: translateY(4px);
 		position: relative;
 
+		& > button.btn:hover::after {
+			background-color: var(--color-primary-500);
+		}
+
 		& > small {
 			line-height: 1.1;
 			text-align: center;
@@ -154,6 +184,31 @@
 			inset-inline: 0;
 			user-select: none;
 			pointer-events: none;
+		}
+	}
+
+	section.work {
+		padding-block-start: 2.5rem;
+	}
+	ul.work-list {
+		list-style: none;
+		display: grid;
+		gap: 1.25rem;
+		padding-block-start: 1.5rem;
+
+		article {
+			display: grid;
+			gap: 0.5rem;
+			padding: 1.25rem;
+			border-radius: 1.25rem;
+
+			&:hover {
+				background-color: color-mix(in oklab, var(--color-white), transparent 95%);
+			}
+
+			p {
+				margin: 0;
+			}
 		}
 	}
 </style>
